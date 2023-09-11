f59032556eeacaf436642ec4e62fc107b9743adf
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-11 21:48:37 +0530

    composition of Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from './components/Header';
+import Main from './components/Main';
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   )
 }
